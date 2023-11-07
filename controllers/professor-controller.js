7c6c2ff2be096dff215537952d215d0291f48808
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-06 21:42:42 -0300

    feat: adicionar edição e exclusão

--- controllers/professor-controller.js
+++ controllers/professor-controller.js
@@ -22,4 +22,29 @@ export const createProfessor = async (req, res) => {
     } 
 }
 
-professor.create();
+export const updateProfessor = async (req, res) => {
+    try {
+        await professor.update(req.body, {
+            where: { matr_professor: req.params.matr_professor }
+        });
+        res.json({
+            message: 'Professor atualizado com sucesso!'
+        });
+    } catch (e) {
+        console.error('Ocorreu um erro ao atualizar um professor: ', e);
+    }
+}
+
+export const deleteProfessor = async (req, res) => {
+    try {
+        await professor.destroy({
+            where: { matr_professor: req.params.matr_professor }
+        });
+        res.json({
+            message: 'Professor deletado com sucesso!'
+        });
+    } catch (e) {
+        console.error('Ocorreu um erro ao deletar um professor: ', e);
+    }
+}
+
