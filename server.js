8faf456f562efac5195ed0b6f8d45ccd6160f512
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-12 20:10:11 -0300

    atualizar server.js

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import db from './config/database.js';
 import dotenv from 'dotenv';
-import routes from './routes/professor-routes.js';
-
+import cors from 'cors';
+import express from 'express';
+import db from './config/database.js';
 import { Curso } from './models/curso-model.js';
-import { Professor } from './models/curso-model.js';
-import { Turma } from './models/curso-model.js';
+import { Professor } from './models/professor-model.js';
+import { Turma } from './models/turma-model.js';
+import { router as professorRoutes } from './routes/professor-routes.js';
+import { router as cursoRoutes } from './routes/curso-routes.js';
+import { router as turmaRoutes } from './routes/turma-routes.js';
 
-const server = express();
-dotenv.config();
-server.use(express.json());
-server.use(cors());
+const server = express()
+server.use(express.json())
+server.use(cors())
+dotenv.config()
 
 try {
   await db.authenticate();
   console.log('Conectado ao banco de dados');
 } catch (e) {
-  console.error('Erro ao conectar ao banco de dados', e);
+  console.error('Erro ao conectar ao banco de dados', e)
 }
+
+server.use('/api', professorRoutes, cursoRoutes, turmaRoutes);
 
 Curso.associate = (models) => {
   Curso.hasMany(models.Professor, {
@@ -31,12 +34,13 @@ Curso.associate = (models) => {
     as: 'turmas',
   })
 
-  Curso.belongsTo(Turma, { foreignKey: 'id_turma', allowNull: true });
-  Curso.belongsTo(Professor, { foreignKey: 'matr_professor', allowNull: true });
+  Curso.belongsTo(Turma, { foreignKey: 'id_turma', allowNull: true })
+  Curso.belongsTo(Professor, { foreignKey: 'matr_professor', allowNull: true })
 }
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT
 server.listen(port, () => {
-  console.log('Servidor online');
+  console.log('Servidor online', port);
 })
 
+export {server};
